d10f0cc47380fe68f2a2eac82b6bd708ce495375
ZapDos7 <ioanna.zapalidi@gmail.com>
2022-12-14 15:01:10 +0200

    implementation of ex10

---
+++ src/main/java/com/zapdos7/behavioraldesignpatterns/exercise10/Pizza.java
@@ -0,0 +1,15 @@
+package com.zapdos7.behavioraldesignpatterns.exercise10;
+
+public class Pizza {
+    public void makeBase() {
+        System.out.println("Mix flour, yeast and salt");
+        System.out.println("Roll out the dough");
+      }
+    
+      public void addToppings(){
+        System.out.println("Add tomato");
+        System.out.println("Add cheese");
+      }
+    
+      public void cook(){}
+}
